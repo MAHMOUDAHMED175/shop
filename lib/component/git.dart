fbd6577ca20631bcb622c4d57c0e962d6f5fc64a
mahmoud <eng.mahmoudahmed@176gmail.com>
2023-01-22 12:57:32 +0200

    first commit

---
+++ lib/component/git.dart
@@ -0,0 +1,89 @@
+//,kjhn
+/*
+                                     git
+
+repositries تختصر الى repo  يعنى الاسم الكبير اللى الكل هيشتغل تحته زى مثلا الشركه وفروعها  الrepo هيا الشركه الام
+Branch ودا الفرع وقسم هعدل الكود جواه
+clone ودي بتعمل نسخ للمشروع علشان انزله عندي واعدل عليه بالكود
+local repo ودى الجهازبتاى فى حاله تعديل الملفات يعنى لسه بشتغل على الاكواد
+remote repo ودى اللى الكل بيحط شغله عليها علشان الكود يتجمع وممكن تكون موقع او سيرفر الشركه مثلا وجا اللى التيم بيدخل عليه علشان لو حد هيعدل حاجه
+commit ودى تعديل معين عملته
+push لتحميل الملفات اللى عدلت عليها الى ال remote repo
+
+
+شويه اوامر للgit
+
+
+1-git status ودي بتقلى ايه اللى بيحصل حالا ايه اللى اتضاف وايه اللى اتمسح       ايه اللى بيحصل يعنى
+
+
+2-git add ودي بتضيف ملف جديد او مجلد جديد انت عملته وممككن اعمل git add * ودى هتضيف الملفات كلها all
+
+
+3-git reset  ودى لو عايز اشيل ملف واعدل عليه واضيفه تانى  وهشيله علشان نسيت اضيف كود مثلا
+
+
+4-git commet -m "type your discription and what did you do"وهنا كدا هبعت الملفات اللى عملتها لل local repo وبكدا انا خلصت تعديل
+
+
+5-git branch بيعرفنى كل الفروع اللى عندى وكل اللbranchs اللى موجوده
+
+
+6-git remote -v ودا الاسم اللى هبعت عليه  وكل التيم هيبعتوا عليه
+
+
+7-git push origin master or main    //origin دا الاسم اللى هبعت عليه اللى الكل هيبعت عليه  ال reomte//main دا اسم ال branch بتاعى االى هبعت منه
+
+
+8-git config --global user.name علشان اعرف الاسم وممكن اعدل على الاسم من خلال ل git config --global user.name "وهنا اكتب الاسم للى عايزه"
+
+
+
+9-git config --global user.email
+
+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
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+kkjb
+
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl
+
+
+
+10-git pull origin master علشان اجيب كل التحديثات الجديده
+
+
+
+11-git remote remove origin علشان تمسح وتبدء من اول وجديد
+
+
+                                                        طريقه الشغل مع تيم
+
+1-checkout master هشوف لو انا مش موجود على الماستر master  وهرجع ليه تانى
+2-update master
+3-create branch from master
+4-code
+5-commit
+6-checkout master
+7-update master
+8-checkout your local branch
+9-merge master with my current branch
+10-push
+11-create pull request ودى مش لللكل دى مثلا لو فى كود شركه منزلاه كمصدر مفتوح للتطوير وانت عدلت عليه وعايز تبعته فبتعمل pull request  وهما يشوفو لو هيقبلو الكود ولا لا
+ */
